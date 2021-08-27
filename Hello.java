cc678157d0271d5604939681fa74b373a848b991
“Nadim” <nadimmansu@gmail.com>
2021-08-27 04:56:00 -0700

    Add : java Hello code

---
+++ Hello.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Hello
+{
+	 public static void main(String[] args) {
+	 	Scanner studentobj = new Scanner(System.in);
+          System.out.println("hello to all");
+	 	
+	 	String name = studentobj.nextLine();
+
+	    System.out.println("Name is"+name);	
+
+	}
+}
